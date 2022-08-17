ba79766a0c10be1dbed9aaad9059805b19caeb96
Damm06 <ghkeka111@gmail.com>
2022-08-17 22:16:06 +0900

    2단계 중간 완료

---
+++ src/main/java/com/baekjoon/level2/Lv_14681.java
@@ -0,0 +1,27 @@
+package com.baekjoon.level2;
+
+import java.io.*;
+
+public class Lv_14681 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a > 0) {
+            if (b > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        }
+        else {
+                if (b > 0) {
+                    System.out.println(2);
+                } else {
+                    System.out.println(3);
+                }
+            }
+        }
+    }
---
+++ src/main/java/com/baekjoon/level2/Lv_2753.java
@@ -0,0 +1,18 @@
+package com.baekjoon.level2;
+
+import java.io.*;
+
+public class Lv_2753 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때
+        //윤년이면 1, 아니면 0을 출력
+        int year = Integer.parseInt(br.readLine());
+        if (year % 4 == 0) {
+            if (year % 100 != 0) System.out.println(1);
+            else if (year % 400 == 0) System.out.println(1);
+            else System.out.println(0);
+        }
+        else System.out.println(0);
+    }
+}
