302a8580f7c03a51e33df724cfbf62f9e61ec1f5
Dam06 <ghkeka111@gmail.com>
2022-10-19 23:31:06 +0900

    feat: 1019 5문제

--- src/main/java/com/baekjoon/level3/Lv_11022.java
+++ src/main/java/com/baekjoon/level3/Lv_11022.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Lv_11021 {
+public class Lv_11022 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-
+        int X = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for (int i = 1; i <= a; i++) {
+
+        for (int i = 1; i <= X; i++) {
             st= new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int sum = x + y;
-            System.out.println("Case #" + i + ": " + sum);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + sum);
         }
         br.close();
     }
