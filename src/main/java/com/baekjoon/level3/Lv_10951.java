845d12f03caba15a82d5d8a6072078bff2db102e
Dam06 <ghkeka111@gmail.com>
2022-10-21 22:18:16 +0900

    feat: 1021 3단계 끝

--- src/main/java/com/baekjoon/level3/Lv_10951.java
+++ src/main/java/com/baekjoon/level3/Lv_10951.java
@@ -5,22 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Lv_10952 {
+public class Lv_10951 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        String str;
 
-        while (true) {
-            st = new StringTokenizer(br.readLine(), " ");
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            if (A == 0 && B == 0) {
-                break;
-            }
-            sb.append((A + B)).append("\n");
+            sb.append(A+B).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
