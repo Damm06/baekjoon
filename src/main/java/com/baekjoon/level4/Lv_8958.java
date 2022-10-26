93f21bfa5c2109fcee926d29ae22daf1a2e668f1
Dam06 <ghkeka111@gmail.com>
2022-10-26 20:11:33 +0900

    feat: 1026 1문제 틀만 잡아놓음

--- src/main/java/com/baekjoon/level4/Lv_8958.java
+++ src/main/java/com/baekjoon/level4/Lv_8958.java
@@ -1,9 +1,13 @@
 package com.baekjoon.level4;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Lv_8958 {
     public static void main(String[] args) throws IOException {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        //OX퀴즈 문제
     }
 }
