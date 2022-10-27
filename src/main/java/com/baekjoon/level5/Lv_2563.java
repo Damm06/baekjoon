1aac8fb5e0f1c5b05f2c60b36e11a59a7ab8dc72
Dam06 <ghkeka111@gmail.com>
2022-10-27 21:37:10 +0900

    feat: 1027 5단계까지 완료

---
+++ src/main/java/com/baekjoon/level5/Lv_2563.java
@@ -0,0 +1,32 @@
+package com.baekjoon.level5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lv_2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = 0; //검은 영역 넓이
+        int N = Integer.parseInt(br.readLine()); //색종이 갯수
+        boolean[][] arr = new boolean[101][101]; //도화지
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            //(x,y)부터 (x+9, y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해줌
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        total++;
+                    }
+                }
+            }
+        }
+        System.out.print(total);
+    }
+}
