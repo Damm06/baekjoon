a8a5a0c46b480c6cd0976dd082d1f2a127974b4a
Damm06 <ghkeka111@gmail.com>
2022-11-03 16:20:38 -0700

    feat: 1103 8단계 3문제 완료

---
+++ src/main/java/com/baekjoon/level8/Lv_10250.java
@@ -0,0 +1,21 @@
+package com.baekjoon.level8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lv_10250 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        //YXX or YYXX, Y는 층수 XX는 방번호 순서
+        //층수는
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        }
+    }
+}
