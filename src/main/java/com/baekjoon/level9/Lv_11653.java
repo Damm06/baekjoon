321dfcf513976fe61a3fea497af300cde7bdbf70
Dam06 <ghkeka111@gmail.com>
2022-11-07 17:15:23 +0900

    feat: 1107 8단계 3문제 완료

---
+++ src/main/java/com/baekjoon/level9/Lv_11653.java
@@ -0,0 +1,24 @@
+package com.baekjoon.level9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lv_11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(N); i++) { //또는 중간꺼 i * i <= N 으로 바꿔도 됨
+            while (N % i == 0) {
+                sb.append(i).append('\n');
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
--- src/main/java/com/baekjoon/level9/Lv_1978.java
+++ src/main/java/com/baekjoon/level9/Lv_1978.java
@@ -6,26 +6,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Lv_1978 {
+    static int cnt;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        cnt = 0;
         int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        primeNumber(); //에라토스테네스의 체 메서드 호출
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-
+            int num = Integer.parseInt(st.nextToken());
+            //num이 지워진 수가 아니면 소수니까 cnt +1
+            if (arr[num] != 0) {
+                cnt++;
+            }
         }
-
+        System.out.print(cnt);
     }
-    //에라토스테네스의 체 방법 = 시간 복잡도 O(nlog(logn))
-    public static boolean[] make_Prime(int n) {
-        boolean[] Prime = new boolean[n + 1]; //0부터 시작하므로 +1
 
-        Prime[0] = true;
-        Prime[1] = true; //0,1은 소수가 아니므로 true
-
-
-
-        return Prime;
+    //에라토스테네스의 체 메서드
+    static void primeNumber() {
+        arr = new int[1001]; //n의 범위가 1000까지니까 1000크기의 배열 선언
+        int n = 1000;
+        //배열 초기화
+        for (int i = 2; i<= n; i++) {
+            arr[i] = i;
+        }
+        //2부터 시작해서 i의 배수들을 배열에서 지워줌
+        for (int i = 2; i <= n; i++) {
+            //이미 지워진 수는 건너뜀
+            if (arr[i] == 0) continue;
+            for (int j = i+i; j <= n; j += i) {
+                arr[j] = 0;
+            }
+        }
     }
 }
